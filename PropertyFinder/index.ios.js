cd0b832b19249951b07ac4cdcec032522df8f156
yesobo <yesobo@Macintosh.local>
2015-04-20 20:59:32 +0200

    Tutorial completed

---
+++ PropertyFinder/index.ios.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+
+var SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', function() {
+  return PropertyFinderApp;
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+#React Native Tutorial: Building Apps with JavaScript
+ref: http://www.raywenderlich.com/99473/introducing-react-native-building-apps-javascript?utm_source=javascriptweekly&utm_medium=email
